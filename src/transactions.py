040784f039b5fefee0b8adb7799184069511e510
dan.toropov <dan.toropov@gmail.com>
2024-05-03 17:44:22 +0300

    add class Transactions

--- src/transactions.py
+++ src/transactions.py
@@ -1,5 +1,21 @@
 class Transactions:
     def __init__(self, dict_: dict):
+        """
+        :param dict_:
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+        """
         self.id = dict_["id"]
         self.state = dict_["state"] == "EXECUTED"
         self.date = dict_["date"]
@@ -10,4 +26,7 @@ class Transactions:
         self.valid = self.validate()
 
     def validate(self) -> bool:
-        pass
+        return True
+
+    def __repr__(self):
+        return f"{self.id},"
